0b75ec9d8a0c6731992af474e74e6e1597baa6d0
suhachakka <suhasinichakka@gmail.com>
2020-01-10 19:39:09 -0600

    added user model

---
+++ src/main/java/org/launchcode/skillstracker/controllers/UserController.java
@@ -0,0 +1,84 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.launchcode.skillstracker.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+
+@RequestMapping("user")
+
+public class UserController {
+
+
+
+        @GetMapping("add")
+
+        public String  displayAddUserForm(Model model){
+
+            model.addAttribute(new User());
+
+            return "/user/add";
+
+        }
+
+
+
+        @PostMapping("add")
+
+        public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+
+            // add form submission handling code here
+
+//        model.addAttribute("user",user);
+
+//        model.addAttribute("verify", verify);
+
+//        model.addAttribute("username", user.getUsername());
+
+//        model.addAttribute("email", user.getEmail());
+
+//        if (verify != null && verify.equals(user.getPassword())) {
+
+//            model.addAttribute("username", user.getUsername());
+
+//            return "/user/index";
+
+//        } else {
+
+//            model.addAttribute("error","Password is not matched");
+
+//            model.addAttribute("username",user.getUsername());
+
+//            model.addAttribute("email", user.getEmail());
+
+//            return "/user/add";
+
+
+
+            if(errors.hasErrors()){
+
+                //    model.addAttribute("user","user");
+
+                return "/user/add";
+
+            }
+
+
+
+                model.addAttribute("name", user.getUsername());
+
+                return "/user/index";
+
+
+
+        }
+
+    }
---
+++ src/main/java/org/launchcode/skillstracker/models/User.java
@@ -0,0 +1,124 @@
+package org.launchcode.skillstracker.models;
+
+
+
+import javax.validation.constraints.Email;
+
+import javax.validation.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+
+import javax.validation.constraints.Size;
+
+
+
+public class User {
+
+    @NotBlank(message="Username can't be empty!")
+
+    @Size(min = 5, max=15, message="UserName length should be between 5 and 15")
+
+    private String username;
+
+    @Email(message="Give Valid Email address!")
+
+    private String email;
+
+    @NotBlank(message="Password can't be empty")
+
+    @Size(min=6, max =7,message="Password should contain minimum 6 characters long!")
+    private String password;
+
+    private String verifyPassword;
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, String verifyPassword) {
+
+        this();
+
+        this.username = username;
+
+        this.email = email;
+
+        this.password = password;
+
+        this.verifyPassword=verifyPassword;
+
+    }
+
+
+
+    public String getUsername() {
+
+        return username;
+
+    }
+
+
+
+    public void setUsername(String username) {
+
+        this.username = username;
+
+    }
+
+
+
+    public String getEmail() {
+
+        return email;
+
+    }
+
+
+
+    public void setEmail(String email) {
+
+        this.email = email;
+
+    }
+
+
+
+    public String getPassword() {
+
+        return password;
+
+    }
+
+    public void setPassword(String password) {
+        this.password=password;
+        checkPassword();
+
+    }
+
+    public String getVerifyPassword() {
+
+        return verifyPassword;
+
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword=verifyPassword;
+        checkPassword();
+
+    }
+
+    private void checkPassword(){
+
+        if((this.password != null) && (this.verifyPassword != null) && ( !(this.password.toLowerCase().equals(this.verifyPassword.toLowerCase()))))
+
+            this.verifyPassword = null;
+
+    }
+
+}
+
+
+
+
+
+
